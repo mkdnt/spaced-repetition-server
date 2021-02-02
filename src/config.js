c95e7386b69ca5913130bdc65a3464a2263c9100
Mike Dent <mikedent85@gmail.com>
2021-02-02 10:20:57 -0500

    deploying...

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL
-    || 'postgresql://MikeDent@localhost/spacedrep',
+    || 'postgres://kkpyncyowhbmxr:128a6cf0f96122fe8f0444c9ca2902d3df9a6aec2897fde8af8da94b5a878825@ec2-34-192-106-123.compute-1.amazonaws.com:5432/d7380l862shhpd',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://MikeDent@localhost/spacedreptest',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
