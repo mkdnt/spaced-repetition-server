22b7094b0008a4b21e263cbb7b691dd64500bf12
Mike Dent <mikedent85@gmail.com>
2021-02-03 11:18:04 -0500

    app.use cors typo

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
-app.use(cors(CLIENT_ORIGIN))
+app.use(cors({origin: CLIENT_ORIGIN}))
 app.use(helmet())
 
 app.use('/api/auth', authRouter)
